3033ffc9997735e0438e9d0fa938d9c46e746e20
dijinx <dimajinx@gmail.com>
2020-10-30 16:31:50 +0700

    Всё переделал

--- src/test/java/Test/TestBase.java
+++ src/test/java/Test/TestBase.java
@@ -16,8 +16,9 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestBase {
-	String userMail = "991mavrodi@gmail.com";
+	String userMail = "1TEST@gmail.com";
 	String userPassword = "mavrodi";
+	String userWrongPassword = "1001001";
 	String userInn = "7707083893";
 	String userKpp = "773601001";
 	String userBik = "123456789";
@@ -29,15 +30,15 @@ public class TestBase {
 	String userTelNumber = "0123456789";
 	String userAddress = "Московская область, г. Москва, ул. Ковальчука, д. 12, кв. 1";
 	String cadastralNumber = "";
-	String SITE_URL = "https://estate.mts.ru/";
+	String SITE_URL = "https://stage.estate.mts.ru/";
 	WebDriver driver;
 	ChromeOptions options;
 	boolean uiModeHeadless = false;
 
 	@BeforeEach //перед каждым тестом
 	public void start() {
-		//System.setProperty("webdriver.chrome.driver", "C:/Users/Dmitriy/IdeaProject/AutotestsEstate/chromedriver.exe"); //локальный файл хромдрайвера
-		WebDriverManager.chromedriver().setup();
+		System.setProperty("webdriver.chrome.driver", "C:/Users/DVGORBU6/IdeaProjects/AutotestsEstateFromeHome/chromedriver.exe"); //локальный файл хромдрайвера
+//		WebDriverManager.chromedriver().setup();
 		options = new ChromeOptions();
 		options.setHeadless(uiModeHeadless);// переключение режима интерфейса
 		driver = new ChromeDriver(options);
@@ -55,7 +56,7 @@ public class TestBase {
 	}
 
 	// форма пустышка для теста----------------------------------------------------------------
-	@Epic("TESTING FOR https://estate.mts.ru/ tasks")
+	@Epic("TESTING FOR https://stage.estate.mts.ru/ tasks")
 	@Feature(value = "ОБРАЗЕЦ")
 	@Severity(SeverityLevel.MINOR)
 	@Description("ОБРАЗЕЦ")
@@ -99,7 +100,7 @@ public class TestBase {
 	}
 //образец шагов для теста, отображение в аллюр каждого шага и вводимых данных
 
-	@Epic("TESTING FOR https://estate.mts.ru/ tasks")
+	@Epic("TESTING FOR https://stage.estate.mts.ru/ tasks")
 	@Feature(value = "Регистрация пользователя")
 	@Severity(SeverityLevel.CRITICAL)
 	@Description("Попытка регистрации пользователя с корректными данными")
@@ -121,7 +122,7 @@ public class TestBase {
 	}
 
 	@Test
-	public void UserRegistration() {
+	public void UserRegistration () throws InterruptedException {
 		//зайти на сайт
 		driver.get(SITE_URL);
 		//нажать на кнопку регистрация
@@ -142,11 +143,13 @@ public class TestBase {
 		driver.findElement(By.id("password-confirm")).sendKeys(userPassword);
 		//нажать кнопку зарегистрироваться
 		driver.findElement(By.xpath("//*[@id=\"kc-form-buttons\"]/input")).click();
+		// пауза сек
+		Thread.sleep(2000);
 		//проверка видимости элемента иконка профиля, статус виден
-		assertTrue(driver.findElement(By.xpath("//*[@id=\"app-header\"]/div[1]/div/div[3]/div/div[3]/span/div")).isDisplayed());
+		assertTrue(driver.findElement(By.xpath("/html/body/div[2]/header/div[1]/div/div[3]/div/div[3]/span/div/img")).isDisplayed());
 	}
 
-	@Epic("TESTING FOR https://estate.mts.ru/ tasks")
+	@Epic("TESTING FOR https://stage.estate.mts.ru/ tasks")
 	@Feature(value = "Авторизация, заполнение данных для вкладок моя организация и мой профиль")
 	@Severity(SeverityLevel.CRITICAL)
 	@Description("Авторизация, заполнение вкладок моя организация и мой профиль с корректными данными")
@@ -171,10 +174,40 @@ public class TestBase {
 		driver.findElement(By.id("kc-login")).click();
 		// пауза сек
 		Thread.sleep(2000);
-		//клик по вкладке моя организация
-		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/aside/div[1]/a[2]/span[2]")).click();
+		//клик по иконке пользователя
+		driver.findElement(By.xpath("/html/body/div[2]/header/div[1]/div/div[3]/div/div[3]/span/div/img")).click();
+		//клик выпадающее меню мой профиль
+		driver.findElement(By.xpath("//a[@href='/profile/user']")).click();
+		//клик по полю фамилия, очистка данных, ввод данных
+		driver.findElement(By.xpath("")).clear();
+		driver.findElement(By.xpath("")).sendKeys(userSurname);
+		//клик по полю имя, очистка данных, ввод данных
+		driver.findElement(By.xpath("")).clear();
+		driver.findElement(By.xpath("")).sendKeys(userName);
+		//клик по полю отчество, очистка данных, ввод данных
+		driver.findElement(By.xpath("")).clear();
+		driver.findElement(By.xpath("")).sendKeys(userPatronymic);
+		//клик по дата рождения, ввод данных
+		driver.findElement(By.xpath("")).clear();
+		driver.findElement(By.xpath("")).sendKeys(userBirth);
+		//клик по мейл, ввод данных
+		driver.findElement(By.xpath("")).clear();
+		driver.findElement(By.xpath("")).sendKeys(userMail);
+		//клик по полю телефон, ввод данных
+		driver.findElement(By.xpath("")).clear();
+		driver.findElement(By.xpath("")).sendKeys(userTelNumber);
+		//клик по полю мейл, ввод данных
+		driver.findElement(By.xpath("")).clear();
+		driver.findElement(By.xpath("")).sendKeys(userAddress);
+		//клик по полю чекбокс
+		driver.findElement(By.xpath("")).click();
+		//клик кнопка сохранить данные
+		driver.findElement(By.xpath("")).click();
+
+
+
 		//клик по полю инн, ввод инфо
-		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/div/main/main/div/form/div[1]/div[1]/div/div/div[1]/input")).sendKeys(userInn);
+		driver.findElement(By.xpath("//input[@placeholder='000000000000']")).sendKeys(userInn);
 		//клик по полю кпп, ввод инфо
 		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/div/main/main/div/form/div[1]/div[2]/div/div/div/input")).sendKeys(userKpp);
 		//клик по кнопке загрузить данные
@@ -195,38 +228,13 @@ public class TestBase {
 		driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/div[2]/div/div/main/main/div[2]/div[1]/div[2]/label/span[1]/span")).click();
 		//нажать кнопку сохранить данные
 		driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/div[2]/div/div/main/main/div[2]/div[1]/button")).click();
-		//клик по вкладке мой профиль
-		driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/div[2]/div/aside/div[1]/a[1]")).click();
-		//клик по полю фамилия, очистка данных, ввод данных
-		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/div/main/main/div/form/div/div[1]/div[1]/div/div/div/input")).clear();
-		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/div/main/main/div/form/div/div[1]/div[1]/div/div/div/input")).sendKeys(userSurname);
-		//клик по полю имя, очистка данных, ввод данных
-		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/div/main/main/div/form/div/div[1]/div[2]/div/div/div/input")).clear();
-		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/div/main/main/div/form/div/div[1]/div[2]/div/div/div/input")).sendKeys(userName);
-		//клик по полю отчество, очистка данных, ввод данных
-		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/div/main/main/div/form/div/div[1]/div[3]/div/div/div/input")).clear();
-		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/div/main/main/div/form/div/div[1]/div[3]/div/div/div/input")).sendKeys(userPatronymic);
-		//клик по дата рождения, ввод данных
-		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/div/main/main/div/form/div/div[1]/div[4]/div/div/div/input")).clear();
-		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/div/main/main/div/form/div/div[1]/div[4]/div/div/div/input")).sendKeys(userBirth);
-		//клик по мейл, ввод данных
-		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/div/main/main/div/form/div/div[1]/div[5]/div/div/div/input")).clear();
-		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/div/main/main/div/form/div/div[1]/div[5]/div/div/div/input")).sendKeys(userMail);
-		//клик по полю телефон, ввод данных
-		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/div/main/main/div/form/div/div[1]/div[6]/div/div/div/input")).clear();
-		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/div/main/main/div/form/div/div[1]/div[6]/div/div/div/input")).sendKeys(userTelNumber);
-		//клик по полю мейл, ввод данных
-		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/div/main/main/div/form/div/div[2]/div/div/input")).clear();
-		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/div/main/main/div/form/div/div[2]/div/div/input")).sendKeys(userAddress);
-		//клик по полю чекбокс
-		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/div/main/main/div/form/div/div[3]/div/label/span[1]/span")).click();
-		//клик кнопка сохранить данные
-		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/div/main/main/div/form/div/button")).click();
+
+
 		//перехват данных текста алерта??
 
 	}
 
-	@Epic("TESTING FOR https://estate.mts.ru/ tasks")
+	@Epic("TESTING FOR https://stage.estate.mts.ru/ tasks")
 	@Feature(value = "Проверка успешной авторизации")
 	@Severity(SeverityLevel.BLOCKER)
 	@Description("Пользователь успешно авторизуется")
